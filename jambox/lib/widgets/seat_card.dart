836a4e50d5cb58cb713974b97f04cd12b571e6ab
Ryan Smith <rnsmith2@gmail.com>
2026-03-29 10:05:52 -0700

    Add missing flutter/services.dart import for HapticFeedback

--- jambox/lib/widgets/seat_card.dart
+++ jambox/lib/widgets/seat_card.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:vibration/vibration.dart';
 import '../models/penalty_box_state.dart';
